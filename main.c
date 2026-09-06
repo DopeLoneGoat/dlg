3a4a159aef999f873814e9cbbf006d1f778f522d
DopeLoneGoat <oleg67ero@gmail.com>
2026-09-07 01:22:49 +0300

    first

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+puts("               ________           ");
+puts("              /        \\          ");
+puts("             /          \\         ");
+puts("            /            \\        ");
+puts("            \\            /        ");
+puts("             \\          /         ");
+puts("              \\________/          ");
+puts("                  ||              ");
+puts("               ___||___           ");
+puts("              |___  ___|          ");
+puts("                  ||              ");
+puts("                  ||              ");
+puts("                  --              ");
+}
